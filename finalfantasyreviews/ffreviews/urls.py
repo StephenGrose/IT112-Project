d8f0bf1ddc6f1ba04ef36512c992c8f615a14651
Stephen G <StephenGrose@users.noreply.github.com>
2021-03-27 18:57:19 -0700

    resubmitting details and initial tests

--- finalfantasyreviews/ffreviews/urls.py
+++ finalfantasyreviews/ffreviews/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.index, name='index'),
+    path('getgames/',views.getgames,name='games'),
+    path('gamedetails/<int:id>',views.gamedetails, name='gamedetails'),
 ]
--- finalfantasyreviews/ffreviews/views.py
+++ finalfantasyreviews/ffreviews/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import ffGame, ffReview
 
 # Create your views here.
 def index (request):
     		return render(request, 'ffreviews/index.html')
+
+def getgames(request):
+	game_list=ffGame.objects.all()
+	return render(request,'ffreviews/games.html',{'game_list': game_list})
+
+def gamedetails(request, id):
+	game=get_object_or_404(ffGame,pk=id)
+	releasedate=game.gameReleaseDate
+	platform=game.gameReleasePlatform
+	summary=game.gameSummary
+	context={
+		'game' : game,
+		'releasedate' : releasedate,
+		'platform' : platform,
+		'summary' : summary,
+	}
+	return render(request, 'ffreviews/gamedetails.html',context=context)
